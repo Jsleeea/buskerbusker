f39e52171fedc426fe2862bfc7f03d5a360dc29a
HONGDUCK <117250797+HONGDUCK@users.noreply.github.com>
2023-01-27 15:22:11 +0900

    require template
    require 했으나 오류발생 후에 수정예정

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 var router_mainPage = require('./router/main_page');
+// var template = require('./lib/template.js');
 
 app.use('/',router_mainPage);
 
