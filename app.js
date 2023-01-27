d1ed83ecd338bac40612f4ce72ce0882d4208ce6
HONGDUCK <117250797+HONGDUCK@users.noreply.github.com>
2023-01-27 11:34:11 +0900

    ROUTER
    라우터 기능 구현

--- app.js
+++ app.js
@@ -1,2 +1,9 @@
-console.log("hello");
-console.log("help");
+var express = require('express');
+var app = express();
+var router_1 = require('./router/router_1');
+
+app.use('/',router_1);
+
+app.listen(3000, () => {
+  console.log('listen t0 3000')
+});
