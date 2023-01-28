ea609c09cffea17e154b09929ada5e7aef06d8c4
leejunsung <brian9777@naver.com>
2023-01-29 03:55:11 +0900

    .create page 접속 안됨
    create page 접속 안됨

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
-var express = require('express');
+var express = require("express");
+var fs = require("fs");
+var qs = require("querystring");
 var app = express();
-var router_mainPage = require('./router/main_page');
+var router_mainPage = require("./router/main_page");
+var router_createPage = require("./router/create_page");
+
 // var template = require('./lib/template.js');
 
-app.use('/',router_mainPage);
-
 app.listen(3000, () => {
-  console.log('listen t0 3000')
+  console.log("listen to 3000");
 });
+
+app.use("/", router_mainPage);
+app.use("/create", router_createPage);
+
+/*
+app.use((req, res, next) => {
+  res.status(/n404).send("Not Found");
+});
+*/
--- router/main_page.js
+++ router/main_page.js
@@ -1,16 +1,15 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 // var template = require('./lib.template.js');
 
-router.use(function(req, res, next) {
+router.use(function (req, res, next) {
   next();
 });
 
-router.get('/', function(req, res) {
-  var title = 'Welcome';
-  var description = 'Hello, Node.js';
-  var html = 
-    `
+router.get("/", function (req, res) {
+  var title = "Welcome";
+  var description = "Hello, Node.js";
+  var html = `
     <!doctype html>
     <html>
       <head>
@@ -18,19 +17,20 @@ router.get('/', function(req, res) {
         <meta charset="utf-8">
       </head>
     <body>
-      <h1><a href="index.html">BUSKER_BUSKER</a></h1>
+      <h1><a href="index.html">BUSKER_BUSKER 질문 게시판</a></h1>
       <ol>
         <li><a href="1.html">HTML</a></li>
         <li><a href="2.html">CSS</a></li>
         <li><a href="3.html">JavaScript</a></li>
       </ol>
+      <a href ="/create">질문 등록하기</a>
       <h2>WEB</h2>
       <p>
       The World Wide Web (abbreviated WWW or the Web) is an information space where documents and other web resources are identified by Uniform Resource Locators (URLs), interlinked by hypertext links, and can be accessed via the Internet.[1] English scientist Tim Berners-Lee invented the World Wide Web in 1989. He wrote the first web browser computer program in 1990 while employed at CERN in Switzerland.[2][3] The Web browser was released outside of CERN in 1991, first to other research institutions starting in January 1991 and to the general public on the Internet in August 1991.
       </p>
     </body>
     </html>
-    `
+    `;
   res.send(html);
 });
 
