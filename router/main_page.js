c1c3d604797a7ea608a9db17e0adadd32a3de0a7
HONGDUCK <117250797+HONGDUCK@users.noreply.github.com>
2023-01-29 12:28:20 +0900

    게시판 및 마이페이지 링크
    게시판 및 마이페이지 링크 걸어둠

--- router/main_page.js
+++ router/main_page.js
@@ -1,4 +1,5 @@
 var express = require('express');
+
 var router = express.Router();
 // var template = require('./lib.template.js');
 
@@ -7,8 +8,6 @@ router.use(function(req, res, next) {
 });
 
 router.get('/', function(req, res) {
-  var title = 'Welcome';
-  var description = 'Hello, Node.js';
   var html = 
     `
     <!doctype html>
@@ -18,15 +17,15 @@ router.get('/', function(req, res) {
         <meta charset="utf-8">
       </head>
     <body>
-      <h1><a href="index.html">BUSKER_BUSKER</a></h1>
+      <h1><a href="/">BUSKER_BUSKER</a></h1>
       <ol>
-        <li><a href="1.html">HTML</a></li>
-        <li><a href="2.html">CSS</a></li>
+        <li><a href="/notice">NOTICE</a></li>
+        <li><a href="/myPage">MyPage</a></li>
         <li><a href="3.html">JavaScript</a></li>
       </ol>
       <h2>WEB</h2>
       <p>
-      The World Wide Web (abbreviated WWW or the Web) is an information space where documents and other web resources are identified by Uniform Resource Locators (URLs), interlinked by hypertext links, and can be accessed via the Internet.[1] English scientist Tim Berners-Lee invented the World Wide Web in 1989. He wrote the first web browser computer program in 1990 while employed at CERN in Switzerland.[2][3] The Web browser was released outside of CERN in 1991, first to other research institutions starting in January 1991 and to the general public on the Internet in August 1991.
+      This is Main Page
       </p>
     </body>
     </html>
