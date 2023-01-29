64297055e27a657565530e4936a99573623aaa56
HONGDUCK <117250797+HONGDUCK@users.noreply.github.com>
2023-01-29 12:28:44 +0900

    23.01.29
    게시판 마이페이지 작성

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var app = express();
 var router_mainPage = require('./router/main_page');
+var router_displayPage = require('./router/noticeDisplay');
+var router_myPage = require('./router/myPage');
+
 // var template = require('./lib/template.js');
 
 app.use('/',router_mainPage);
+app.use('/notice',router_displayPage);
+app.use('/myPage',router_myPage);
 
 app.listen(3000, () => {
   console.log('listen t0 3000')
