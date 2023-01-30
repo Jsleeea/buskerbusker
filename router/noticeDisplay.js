39b716b3944ec41f3770d012999b7724d7df26a4
leejunsung <brian9777@naver.com>
2023-01-31 03:53:10 +0900

    답글 거의 완성
    약간의 오류 있음 answer1 답글은 기록 안됨

--- router/noticeDisplay.js
+++ router/noticeDisplay.js
@@ -13,6 +13,37 @@ router.use(function (req, res, next) {
 router.get("/:pageID", function (request, response) {
   fs.readdir("./data/noticeData", function (err, filelist) {
     var filteredId = path.parse(request.params.pageID).base;
+    fs.readdir(
+      `./data/${filteredId}/answerData`,
+      function (err, answer_filelist) {
+        fs.readFile(
+          `./data/noticeData/${filteredId}`,
+          "utf-8",
+          function (err, description) {
+            var title = request.params.pageID;
+            var list = template.list(filelist);
+            var answer_list = template.answer_list(answer_filelist);
+            var html = template.HTML(
+              title,
+              list,
+              `
+            <div>답변:</div>
+            ${answer_list}
+            <a href='/answer/${filteredId}'>답변하기</a>`,
+              `<h2>${title}</h2>${description}<br><br>`
+            );
+            response.send(html);
+          }
+        );
+      }
+    );
+  });
+});
+
+/*
+router.get("/:pageID", function (request, response) {
+  fs.readdir("./data/noticeData", function (err, filelist) {
+    var filteredId = path.parse(request.params.pageID).base;
 
     fs.readFile(
       `./data/noticeData/${filteredId}`,
@@ -23,7 +54,9 @@ router.get("/:pageID", function (request, response) {
         var html = template.HTML(
           title,
           list,
-          `<a href='/answer/${filteredId}'>답변하기</a>`,
+          `
+          <div>답변:</div>
+          <a href='/answer/${filteredId}'>답변하기</a>`,
           `<h2>${title}</h2>${description}<br>`
         );
         response.send(html);
@@ -31,5 +64,5 @@ router.get("/:pageID", function (request, response) {
     );
   });
 });
-
+*/
 module.exports = router;
