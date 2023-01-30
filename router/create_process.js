d7f841298edcaaa69d3c8e7cb95f038e19430792
leejunsung <brian9777@naver.com>
2023-01-30 19:20:19 +0900

    create page
    create page 완성

--- router/create_process.js
+++ router/create_process.js
@@ -9,43 +9,21 @@ router.use(function (req, res, next) {
   next();
 });
 
-router.get("/", function (req, res) {
-  var title = "Welcome";
-  var html = `
-    <!doctype html>
-    <html>
-      <head>
-        <title>BUSKERBUSKER - Welcome</title>
-        <meta charset="utf-8">
-      </head>
-    <body>
-      <h1><a href="/">BUSKER_BUSKER 질문 게시판</a></h1>
-      <form action="/create_process" method="post">
-      <p><input type="text" name="title" placeholder="title"></p>
-      <p>
-        <textarea name="description" placeholder="description"></textarea>
-      </p>
-      <p>
-        <input type="submit">
-      </p>
-    </form>
-    </body>
-    </html>
-    `;
-  res.send(html);
-});
-
-/*
-router.post("/create_process", function (request, response) {
+router.post("/", function (request, response) {
   var post = request.body;
   var title = post.title;
   var description = post.description;
-  fs.writeFile(`./data/${title}`, description, "utf8", function (err) {
-    response.writeHead(302, { Location: `/?id=${title}` });
-    response.end();
-  });
+  fs.writeFile(
+    `./DATA/noticeData/${title}`,
+    description,
+    "utf8",
+    function (err) {
+      //response.writeHead(302, { Location: `/?id=${title}` });
+      response.writeHead(302, { Location: "/" });
+      response.end();
+    }
+  );
 });
-*/
 
 module.exports = router;
 /*
