27fde90ed3201e06db040fb567072956c2c9d8a2
HONGDUCK <117250797+HONGDUCK@users.noreply.github.com>
2023-02-02 16:44:06 +0900

    notice database 구축

--- lib/loginData.sql
+++ lib/loginData.sql
@@ -18,9 +18,29 @@ CREATE TABLE `userData` (
 -- Dumping data for table `userData`
 --
 
-INSERT INTO `userData` VALUES (1,'HONG','hong');
-INSERT INTO `userData` VALUES (2,'DUCK','duck');
-INSERT INTO `userData` VALUES (3,'HWA','hwa');
+INSERT INTO `userData` VALUES (1,'HONG','hong','sjan');
+INSERT INTO `userData` VALUES (2,'DUCK','duck','djfu');
+INSERT INTO `userData` VALUES (3,'HWA','hwa','dnjdy');
+
+--
+-- Table structure for table `noticeData`
+--
+
+CREATE TABLE `noticeData` (
+    `num` int(11) AUTO_INCREMENT,
+    `author` varchar(20) NOT NULL,
+    `text` text NOT NULL,
+    `created` datetime NOT NULL,
+    PRIMARY KEY (`num`);
+);
+
+--
+-- Dumping data for table `noticeData`
+--
+
+INSERT INTO `userData` VALUES (NULL,'HONG','my name is hong','now()');
+INSERT INTO `userData` VALUES (NULL,'DUCK','my name is duck','now()');
+INSERT INTO `userData` VALUES (NULL,'HWA','my name is hwa','now()');
 
 /* 참고용 주석
 --
